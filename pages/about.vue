7d8bbdbbd1d0414aeca79fecad34461ab799582c
Roberto Vasquez-Angel <rva@beegoodit.de>
2023-03-14 20:15:25 +0100

    BFF-0 feat: add about page and navigation

--- pages/about.vue
+++ pages/about.vue
@@ -27,7 +27,7 @@
         </ion-toolbar>
       </ion-header>
       <ion-content class="ion-padding">
-        <h1>Home</h1>
+        <h1>About</h1>
       </ion-content>
     </ion-page>
   </div>
