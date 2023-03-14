fcfb61f480aa1a3bff9de9368c8b87a7cbd43820
Roberto Vasquez-Angel <rva@beegoodit.de>
2023-03-14 21:25:48 +0100

    BFF-0 feat: configure nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/ionic']
+  modules: ['@nuxtjs/ionic'],
+  ssr: false,
+  mode: 'universal',
+  target: 'static',
+  generate: {
+    fallback: true
+  }
 })
